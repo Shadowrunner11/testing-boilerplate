a2a9b26d22859cadb4c5310846509b47b4e6127d
Antony Palomino <antony.leo12agosto@gmail.com>
2022-11-26 07:02:41 -0500

    Add test enpoints

--- test/e2e/endpoints.test.ts
+++ test/e2e/endpoints.test.ts
@@ -3,6 +3,18 @@ import server from '../../src/server'
 
 const appTest = supertest(server)
 
+let _id = ''
+
+const taskInsert = {
+  title: 'Put endpoint', 
+  description: 'Task reference to put method',
+  status: 'Pending'
+}
+const taskUpdate = {
+  _id,
+  status: 'Close'
+}
+
 describe('endpoints', () => {
   it('is service available', function (done) {
     appTest
@@ -10,7 +22,28 @@ describe('endpoints', () => {
       .expect('Content-Type', /json/)
       .expect(200, done)
   })
-
+  it('Put one record', function(done){
+    appTest
+      .put('/api/v1/task/upsertTask')
+      .send({body: taskInsert})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(({body})=>{
+        body.title = taskInsert.title
+      })
+      .then(({body}) => {_id = body._id; done()})
+      
+  })
+  it('Update one record', function(done){
+    appTest
+      .put('/api/v1/task/upsertTask')
+      .send({body: taskUpdate})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(({body})=>{
+        body.status = taskUpdate.status
+      })
+  })
   it('list all', function (done) {
     appTest
       .get('/api/v1/task')
@@ -18,5 +51,14 @@ describe('endpoints', () => {
       .expect(200)
       .then(({body}) => {console.log(body); done()})
   })
-
+  it('delete record', function (done) {
+    appTest
+      .delete('/api/v1/task')
+      .send({body: {_id}})
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(({body})=>{
+        body.status = 'Deleted'
+      })
+  })
 })
